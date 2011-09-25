f2dc63b893a6acf03c3c35ca3c1ce29306b2667d
Sam <sam@cruft.co>
2011-09-25 15:36:44 +0100

    Added comments

--- test.py
+++ test.py
@@ -6,16 +6,22 @@ import json
 import time
 import random
 
+# Possible types for out objects
 types = ['foo', 'bar', 'baz']
 
+# Dummy class
 class Dummy(dict):
     def __init__(self):
+
+# Set some values
         self['ts'] = time.time()
         self['value'] = random.randint(0, 99)
         self['type'] = random.choice(types)
+        self['class'] = self.__class__.__name__.lower()
 
         self.set_dimensions()
 
+# Set an embedded dict
     def set_dimensions(self):
         d = {}
         d['x'] = random.randint(0, 10)
@@ -23,25 +29,37 @@ class Dummy(dict):
 
         self['dimensions'] = d
 
+# Save to the DB
     def save(self, clxn):
         clxn.insert(self)
 
+# Get our config
 yamlpath = 'config/config.yaml'
 f = open(yamlpath, 'r')
 y = yaml.load(f)
 d = y['mongo']
 
+# Mongo connection
 cnxn = Connection(d['host'], d['port'])
 
+# A Mongo DB
 db = cnxn[d['db']]
+
+# We might wanna authenticate
 #db.authenticate(d['user'], d['pass'])
 
+# A Mongo collection
 c = db[d['collection']]
 
+# Clean up before we start
+c.drop()
+
+# Create and insert some of our objects
 for i in range(10):
     d = Dummy()
     d.save(c)
 
+# Find all of the entries of type 'foo'
 d = c.find({'type': 'foo'})
 for e in list(d):
     print e

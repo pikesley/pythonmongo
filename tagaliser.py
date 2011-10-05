3bee03415a547dc69e7f9f71dc81f7f127b217d5
Sam <sam@cruft.co>
2011-10-05 20:45:51 +0100

    Just playing around with some mp3s

---
+++ tagaliser.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+from pymongo import Connection
+import yaml
+import tagpy
+import sys
+import hashlib
+import os
+import re
+
+class Track(dict):
+    def __init__(self, path):
+        self['path'] = path
+        self.fileref = tagpy.FileRef(self['path'])
+
+        self.tags = self.fileref.tag()
+
+        self['artist'] = self.tags.artist
+        self['track'] = self.tags.track
+        self['title'] = self.tags.title
+        self['album'] = self.tags.album
+        self['year'] = self.tags.year
+        self['md5'] = hashlib.md5(self['path']).hexdigest()
+        self['file_type'] = self['path'].split('.')[-1]
+
+def populate(drop_first=True):
+    if drop_first:
+        clxn.drop()
+
+    for d in os.walk(y['paths']['music']):
+        for t in d[2]:
+            if t.endswith("ogg") or t.endswith("mp3"):
+                track = Track(os.path.join(d[0], t))
+                clxn.insert(track)
+
+    t = clxn.find_one()
+    for k in t.keys():
+        print "Creating index '%s'" % (k)
+        clxn.create_index(k)
+
+yamlpath = 'config/config.yaml'
+f = open(yamlpath, 'r')
+y = yaml.load(f)
+d = y['mongo']
+
+cnxn = Connection(d['host'], d['port'])
+
+db = cnxn[d['db']]
+clxn = db[d['collection']]
+
+if __name__ == '__main__':
+    populate()
+#    t = clxn.find({'artist': 'The Beatles'})
+#   for x in list(t):
+#       print x
--- test.py
+++ test.py
@@ -1,75 +1,30 @@
 #!/usr/bin/env python
 
 from pymongo import Connection
-from pymongo import ASCENDING, DESCENDING
-
-import re
 import yaml
-import json
-import time
-import random
 
-# Possible types for out objects
-types = ['foo', 'bar', 'baz']
-
-# Dummy class
-class Dummy(dict):
-    def __init__(self):
-
-# Set some values
-        self['ts'] = time.time()
-        self['value'] = random.randint(0, 99)
-        self['type'] = random.choice(types)
-        self['class'] = self.__class__.__name__
-
-        self.set_list()
-        self.set_dimensions()
-
-    def set_list(self):
-        l = []
-        for i in range(5):
-            l.append(random.randint(0, 9999))
-
-        self['list'] = l
-
-# Set an embedded dict
-    def set_dimensions(self):
-        d = {}
-        d['x'] = random.randint(0, 9)
-        d['y'] = random.randint(0, 9)
-
-        self['dimensions'] = d
-
-# Get our config
 yamlpath = 'config/config.yaml'
 f = open(yamlpath, 'r')
 y = yaml.load(f)
 d = y['mongo']
 
-# Mongo connection
 cnxn = Connection(d['host'], d['port'])
 
-# A Mongo DB
 db = cnxn[d['db']]
-
-# We might wanna authenticate
 #db.authenticate(d['user'], d['pass'])
 
-# A Mongo collection
-c = db[d['collection']]
+c = db['foo']
 
-# Clean up before we start
-c.drop()
+x = {'name': 'Perl', 'type': 'Degu'}
+y = {'name': 'Ruby', 'type': 'Degu'}
 
-# Create and insert some of our objects
-for i in range(10):
-    d = Dummy()
-    c.insert(d)
+c.insert(x)
+c.insert(y)
+c.save(x)
+c.save(y)
 
-# Create an index
-c.create_index("ts", ASCENDING)
+print list(c.find())
 
-d = c.find({'value': {'$gt': 75}})
-#d = c.find({'type': re.compile('ba')})
-for e in list(d):
-    print e
+v = list(c.find(fields='_id'))
+for u in v:
+    c.remove(u['_id'])
